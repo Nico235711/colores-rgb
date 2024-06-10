9866d7e5deb7312124079d1608c9406c22d36067
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-06-09 21:46:01 -0300

    terminando con el js

---
+++ script.js
@@ -0,0 +1,52 @@
+const inputRojo = document.getElementById('input-rojo')
+const textoRojo = document.getElementById('texto-rojo')
+
+const inputVerde = document.getElementById('input-verde')
+const textoVerde = document.getElementById('texto-verde')
+
+const inputAzul = document.getElementById('input-azul')
+const textoAzul = document.getElementById('texto-azul')
+
+const colores = [inputRojo, inputVerde, inputAzul]
+
+let rojo = inputRojo.value
+let verde = inputVerde.value
+let azul = inputAzul.value
+
+colores.forEach(color => {
+  color.addEventListener("input", event => {
+    if (color.id.includes("rojo")) {
+      rojo = event.target.value
+      textoRojo.textContent = rojo
+    }
+    if (color.id.includes("verde")) {
+      verde = event.target.value
+      textoVerde.textContent = verde
+    }
+    if (color.id.includes("azul")) {
+      azul = event.target.value
+      textoAzul.textContent = azul
+    }
+
+    document.body.style.backgroundColor = `rgb(${rojo}, ${verde}, ${azul})`
+
+  })
+});
+
+// inputRojo.addEventListener("input", event => {
+//   rojo = event.target.value
+//   textoRojo.textContent = rojo
+//   document.body.style.backgroundColor = `rgb(${rojo}, ${verde}, ${azul})`
+// })
+
+// inputVerde.addEventListener("input", event => {
+//   verde = event.target.value
+//   textoVerde.textContent = verde
+//   document.body.style.backgroundColor = `rgb(${rojo}, ${verde}, ${azul})`
+// })
+
+// inputAzul.addEventListener("input", event => {
+//   azul = event.target.value
+//   textoAzul.textContent = azul
+//   document.body.style.backgroundColor = `rgb(${rojo}, ${verde}, ${azul})`
+// })
